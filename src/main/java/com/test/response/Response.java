ab5f2b3e69f6da5a90f7922782bdeb76ac353031
swapnilborate70 <swapnil.borate@revdau.com>
2024-04-24 22:14:38 +0530

    "6.2"

--- src/main/java/com/test/response/Response.java
+++ src/main/java/com/test/response/Response.java
@@ -7,6 +7,7 @@ import io.vertx.ext.web.RoutingContext;
 
 public class Response {
 
+
   public static void jsonResponse(RoutingContext rc, int statusCode, JsonObject responseBody) {
     rc.response().setStatusCode(statusCode)
       .putHeader(ResponseConstants.CONTENT_TYPE_HEADER, ResponseConstants.APPLICATION_JSON);
